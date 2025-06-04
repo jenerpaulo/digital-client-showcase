bc7e255ae9a60acdd0833782187f84679d920c08
jenerpaulo <jenerlauriano@gmail.com>
2025-06-04 18:23:33 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -531,7 +531,7 @@ const Index = () => {
               <CardContent className="p-6">
                 <p className="text-gray-600 dark:text-gray-300 text-base mb-4">
                   Neste modelo, incluiremos uma proposta diferenciada para que a Divino Amor esteja 
-                  presente em nossa nova rede, a Católicos Online. Veja exemplo da página <a href="https://www.exemplo.com" target="_blank">aqui.</a>
+                  presente em nossa nova rede, a Católicos Online. Veja exemplo da página <a href="https://catolicos.online/pages/DivinoAmor/products" target="_blank" style="color: blue;">aqui.</a>
 
                 </p>
                 <Badge variant="outline" className="text-purple-600 dark:text-purple-400 dark:border-purple-600 text-base">Premium</Badge>
